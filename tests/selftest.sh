81748317e67ca42c7d24680661d8f59bc5bc6474
Blaine Schmeisser <blainesch@gmail.com>
2014-12-19 21:06:51 -0600

    Update links.... I think

--- tests/selftest.sh
+++ tests/selftest.sh
@@ -1,7 +1,3 @@
-#!/usr/bin/env bash
 cd "$(dirname $0)/.."
-# We're calling intern-X.js directly here so that this script can be shared by
-# master and geezer, but intern should normally be started using the links
-# created by npm in node_modules/.bin.
-node_modules/intern/bin/intern-client.js config=tests/selftest.intern reporters=intern-selftest/lib/reporters/combined $@
-node_modules/intern/bin/intern-runner.js config=tests/selftest.intern reporters=intern-selftest/lib/reporters/combined $@
+node_modules/.bin/intern-client config=tests/selftest.intern reporters=intern-selftest/lib/reporters/combined $@
+node_modules/.bin/intern-runner config=tests/selftest.intern reporters=intern-selftest/lib/reporters/combined $@
