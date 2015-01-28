0072506a2a30b62c8db6b62e0201ea799f7e946c
Adam Faur <adam.faur@vitals.com>
2015-01-28 11:00:04 -0600

    Comment test.

--- tests/functional/search.js
+++ tests/functional/search.js
@@ -4,18 +4,31 @@ define([
   'dojo/node!leadfoot/helpers/pollUntil'
 ], function (bdd, expect, pollUntil) {
   with(bdd) {
+    // New group of tests called `Search`
     describe('Search', function() {
+      // New nested group of tests called `Autocomplete`
       describe('Autocomplete', function() {
+        // New test called `foo`
         it('foo', function() {
+          // remote is the remote request object
           return this.remote
+          // Load the vitals website
           .get('http://www.vitals.com')
+          // Find the query input box
           .findById('q')
+            // Click the input box
             .click()
             .type('todd')
+            // Finish typing
             .end()
+          // Pause/wait
+          // Using jquery to check .masthead-main-dropdown visibility until it is shown
           .then(pollUntil('return (jQuery(".masthead-main-dropdown:visible").length > 0 ? true : null);', 30000))
+          // Once the drop down is visible then find the first result span under provider
           .findByCssSelector('.masthead-main-dropdown .provider.first span.name')
+          // Get the text value of the span (providers name)
           .text()
+          // Use the value we got from the span to compare it with what we expect to be there
           .then(function(text) {
             expect(text).to.match(/todd/i)
           });
@@ -24,3 +37,4 @@ define([
     });
   }
 });
+
--- tests/selftest.sh
+++ tests/selftest.sh
@@ -1,3 +1,7 @@
 cd "$(dirname $0)/.."
+
+export SAUCE_USERNAME=afaur
+export SAUCE_ACCESS_KEY=b5399ad8-270f-44ce-b29f-729d3020333c
+
 node_modules/.bin/intern-client config=tests/selftest.intern reporters=intern-selftest/lib/reporters/combined $@
 node_modules/.bin/intern-runner config=tests/selftest.intern reporters=intern-selftest/lib/reporters/combined $@
